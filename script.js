9746f0aacc0ca8e80c1fa4471df71e5e6519d4e9
Ada Kalkan <ada.aslan@codeck.com.tr>
2022-06-06 00:55:23 +0300

    js practice

---
+++ script.js
@@ -0,0 +1,52 @@
+// console.log("hellooo");
+// var job;
+// console.log(job);
+// job = "student";
+// console.log("job");
+
+// var schoolName, hasCourse;
+// schoolName = "akademi";
+// hasCourse = "ture";
+
+// console.log(schoolName + "okul" + 30 + hasCourse);
+// //typecoercion
+
+// var schoolNumber = "twenty two";
+
+// console.log(schoolNumber);
+
+// alert(schoolNumber);
+
+// var age = prompt("how old is he?");
+// console.log("alex is" + age + "years old");
+
+// var totalDistance,distancealex;
+// totalDistance=20;
+// distancealex=totalDistance - 12;
+// console.log(distancealex);
+
+// var grade;
+
+// grade = 55;
+// if (grade < 40) {
+//   console.log("you can not go to next class");
+// } else if (grade > 40) {
+//   console.log("you can go to next class");
+// }
+//conditional statements
+
+// var grade = 42;
+// grade < 45 ? console.log("you can not next") : console.log("you passed");
+
+//ternary operator ? ise , : değilse
+
+var gradeAlex = (90 + 80 + 95) / 3;
+var gradeSara = (89 + 76 + 98) / 3;
+var gradeNancy = (42 + 98 + 83) / 3;
+console.log("alex average grade", gradeAlex);
+console.log("sara average grade", gradeSara);
+console.log("nancy average grade", gradeNancy);
+
+if (gradeAlex > gradeSara && gradeAlex > gradeNancy) {
+  console.log("alex has the highest grade", gradeAlex);
+}
